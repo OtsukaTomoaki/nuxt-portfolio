a2458f95e4c0d7cb2e79ff342c699f0b66b2f17d
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-12-10 18:47:00 +0900

    ブログのread moreの詳細を追加

---
+++ pages/blog/[id].vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <nuxt-img :src="data.image" alt="blog-image" format="webp" />
+    </div>
+    <div>
+      <div>
+        <h1>{{ data.title }}</h1>
+        <p>{{ data.date }}</p>
+        <ContentDoc />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const path = useRoute().path
+const { data } = await useAsyncData(path, () => {
+  return queryContent(path).findOne()
+})
+
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <h1>Blog</h1>
+      <p>エンジニアの日常生活をお届けします</p>
+      <div v-for="blog in data" :key="blog.id">
+        <div>
+          <h3>{{ blog.title }}</h3>
+          <p>{{ blog.excerpt }}</p>
+          <p>{{ blog.date  }}</p>
+          <NuxtLink :to="blog._path">ReadMore</NuxtLink>
+        </div>
+        <div>
+          <nuxt-img :src="blog.image" alt="blog-image" format="webp" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("blogQuery", () => {
+  return queryContent("/blog").sort({ id: -1 }).find()
+})
+
+</script>
